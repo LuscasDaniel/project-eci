63290839695a7453b4e2a21379d735266d26b10d
tiofeels <lucas.daniel42@hotmail.com>
2021-04-10 21:06:01 -0300

    Test

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
 
 app.config["DEBUG"] = True
 
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+def index():
+    url_for('static', filename='/static/logo.png')
+    return render_template('Index.html')
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
