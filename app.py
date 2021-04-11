790bf0cd1e9497de09f96c862beb5539e1f4a3a5
tiofeels <lucas.daniel42@hotmail.com>
2021-04-10 22:12:55 -0300

    test

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask, escape, request, render_template, url_for
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 
 
 app.config["DEBUG"] = True
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    #url_for('static', filename='/static/logo.png')
-    return render_template('Index.html')
-
+    url_for('static', filename='/static/logo.png')
+    return render_template('/Index.html')
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
